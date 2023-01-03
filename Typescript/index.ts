9dde8c134f64fb7c5d6ce5b795e8da5a19716986
Vish05 <vishal.tanna2010@gmail.com>
2023-01-03 15:57:47 +0530

    Plain Typescript

---
+++ Typescript/index.ts
@@ -0,0 +1,18 @@
+import fs from "fs";
+
+interface Result {
+  results: {
+    id: number;
+    name: string;
+    job: string;
+  }[];
+}
+
+const printJobs = (results: Result) => {
+  results?.results.forEach(({ job }) => {
+    console.log(job);
+  });
+};
+
+const data: Result = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+printJobs(data);
